f074b4578669dd265479cef58d924feb69841576
kongarasan <kongarasan49@gmail.com>
2024-04-27 23:33:01 +0530

    Navbar push

--- frontend/src/components/Navbar/Navbar.jsx
+++ frontend/src/components/Navbar/Navbar.jsx
@@ -1,19 +1,32 @@
 import React from 'react'
 import './Navbar.css'
-import {assets} from "../../assets/assets"
+import { assets } from "../../assets/assets"
 
 const Navbar = () => {
-  return (
-    <div className='navbar'>
-        <img src={assets.logo} className='logo'></img>
-        <ul className='navbar-menu'></ul>
-        <li>Home</li>
-        <li>Menu</li>
-        <li>Mobile-App</li>
-        <li>Contact Us</li>
+    return (
+        <div className='navbar'>
+            <img src={assets.logo} className='logo'></img>
+            <ul className='navbar-menu'>
+                <li>Home</li>
+                <li>Menu</li>
+                <li>Mobile-App</li>
+                <li>Contact Us</li>
+            </ul>
 
-    </div>
-  )
+            <div className='navbar-right'>
+                <img src={assets.search_icon}></img>
+                <div className='navbar-search-icon'>
+                    <img src={assets.basket_icon}></img>
+                    <div className='dot'></div>
+
+                </div>
+                <button>Sign in</button>
+
+            </div>
+
+
+        </div>
+    )
 }
 
 export default Navbar
