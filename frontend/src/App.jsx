d9a4d4d5c9000a208e41fd7b84a6cc14f74d4303
kongarasan <kongarasan49@gmail.com>
2024-05-01 00:19:06 +0530

    Header Push

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,10 +1,20 @@
 import React from 'react'
 import Navbar from './components/Navbar/Navbar'
+import { Route, Routes } from 'react-router-dom'
+import Home from "../src/pages/Home/Home"
+import Cart from './pages/Card/Cart'
+import PlaceOrder from './pages/PlaceOrder/PlaceOrder'
 
 const App = () => {
   return (
     <div className='app'>
       <Navbar/>
+      <Routes>
+<Route path='/' element={<Home/>}></Route>
+<Route path='/cart' element={<Cart/>}></Route>
+<Route path='/order' element={<PlaceOrder/>}></Route>
+
+      </Routes>
     </div>
   )
 }
