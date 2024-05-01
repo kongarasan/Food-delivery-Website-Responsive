2d8faec6b7970e552088296c7b921f1dfa8683e9
kongarasan <kongarasan49@gmail.com>
2024-05-01 13:46:11 +0530

    Complete Header

--- frontend/src/components/Navbar/Header/Header.jsx
+++ frontend/src/components/Navbar/Header/Header.jsx
@@ -3,7 +3,13 @@ import "./Header.css"
 
 const Header = () => {
   return (
-    <div className='Header'></div>
+    <div className='Header'>
+      <div className='header-contents'>
+      <h2>Order your favourite food here</h2>
+      <p>Choose from a diverse menu featuring a delectable array of dishes crafted with the finest ingredients and culinary expertise.Our mission is to satisfy your cravings and elevate your dining experience, one delicious mealat a time.  </p>
+  <button>View Menu</button>
+      </div>
+    </div>
   )
 }
 
